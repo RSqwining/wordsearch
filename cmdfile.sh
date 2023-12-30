9fa3e50e90bc6d99240e9fffd41e4107f567db59
RSqwining <147663623+RSqwining@users.noreply.github.com>
2023-12-30 15:15:06 -0800

    Update and rename cmdfile.txt to cmdfile.sh

--- cmdfile.sh
+++ cmdfile.sh
@@ -1,10 +1,5 @@
 
-
 { printf "[\n"; sed "s/.\{17\}/&\n/g" file.txt | sed 's/./&", "/g' | sed 's/, "$/],/g' | sed 's/^/["/g' | sed '$s/.$//g'; printf "]\n"; } > file1.txt
 
 
-
 printf "[\n"; sed -e "s/.\{17\}/&\n/g" file.txt | sed "s/./& /g" | sed 's/ /", "/g' | sed 's/, "$/],/g' | sed 's/^/["/g' | sed '$ s/.$//'; printf "]\n"
-
-
-
